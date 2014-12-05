db3ec8e97793b24dac0fa8191efd217f4f109cf4
b.naskov <boni.mislyashki@gmail.com>
2014-12-05 14:27:52 +0200

    bank edited

--- OnlineBanking/src/bank/Bank.java
+++ OnlineBanking/src/bank/Bank.java
@@ -21,6 +21,7 @@ public class Bank implements IBank {
 
 	private static Bank instance = null;
 
+	// remove singleton
 	private Bank() {
 	}
 
@@ -30,15 +31,15 @@ public class Bank implements IBank {
 		}
 		return instance;
 	}
-	
+
 	public Map<String, IClient> getUsers() {
 		return users;
 	}
-	
+
 	public Map<String, Account> getAccounts() {
 		return allAccounts;
 	}
-	
+
 	public void addClientToBank(IClient client) {
 		users.put(((Client) client).getIdNumber(), client);
 	}
@@ -46,88 +47,114 @@ public class Bank implements IBank {
 	public void displayAllDeposits() {
 
 	}
-	
+
 	public void addAccountsInBank(String iban, Account account) {
-		if(iban != null && account != null)
+		if (iban != null && account != null)
 			allAccounts.put(iban, account);
 	}
 
+	// void exception
 	public boolean transferMoney(IClient client, String initiatorIban,
 			String recipientIban, String reasonForTransfer, double amount) {
-		if(allAccounts.containsKey(initiatorIban)) {
-			if(allAccounts.get(initiatorIban).getAmount() >= amount) {
-				if(allAccounts.containsKey(recipientIban)) {
+		if (allAccounts.containsKey(initiatorIban)) {
+			if (allAccounts.get(initiatorIban).getAmount() >= amount) {
+				if (allAccounts.containsKey(recipientIban)) {
 					allAccounts.get(recipientIban).addMoneyToAccount(amount);
-					allAccounts.get(initiatorIban).removeMoneyFromAccount(amount);
+					allAccounts.get(initiatorIban).removeMoneyFromAccount(
+							amount);
 					return true;
-				}
-				else {
-					System.out.println("There's no account with this IBAN in the bank!"); //for testing
+				} else {
+					System.out
+							.println("There's no account with this IBAN in the bank!"); // for
+																						// testing
 					return false;
 				}
-			}
-			else {
-				System.out.println("You have not enough money for this transaction!"); //for testing
+			} else {
+				System.out
+						.println("You have not enough money for this transaction!"); // for
+																						// testing
 				return false;
 			}
-		}
-		else {
-			System.out.println("You don't have an account with this IBAN! Please enter a valid IBAN!");
+		} else {
+			System.out
+					.println("You don't have an account with this IBAN! Please enter a valid IBAN!");
 			return false;
 		}
 
 	}
 
-	public void openAccount(IClient client, String initiatorIban,
-			double amount) {
-		if(allAccounts.containsKey(initiatorIban)) {
-			if(allAccounts.get(initiatorIban).getAmount() >= amount) {
-				if(!client.getClientAccounts().containsKey("CurrentAccount")) {
-					client.getClientAccounts().put("CurrentAccount", new HashMap<String, ArrayList<Account>>());
+	public void openAccount(IClient client, String initiatorIban, double amount) {
+		if (allAccounts.containsKey(initiatorIban)) {
+			if (allAccounts.get(initiatorIban).getAmount() >= amount) {
+				if (!client.getClientAccounts().containsKey("CurrentAccount")) {
+					client.getClientAccounts().put("CurrentAccount",
+							new HashMap<String, ArrayList<Account>>());
 				}
-				if(!client.getClientAccounts().get("CurrentAccount").containsKey("CurrentAccountBGN")) {
-					client.getClientAccounts().get("CurrentAccount").put("CurrentAccountBGN", new ArrayList<Account>());
+				if (!client.getClientAccounts().get("CurrentAccount")
+						.containsKey("CurrentAccountBGN")) {
+					client.getClientAccounts().get("CurrentAccount")
+							.put("CurrentAccountBGN", new ArrayList<Account>());
 				}
-				client.getClientAccounts().get("CurrentAccount").get("CurrentAccountBGN").add(new CurrentAccountBGN(amount));
+				client.getClientAccounts().get("CurrentAccount")
+						.get("CurrentAccountBGN")
+						.add(new CurrentAccountBGN(amount));
 				allAccounts.get(initiatorIban).removeMoneyFromAccount(amount);
+			} else {
+				System.out
+						.println("You have not enough money to open this Account!"); // for
+																						// testing
 			}
-			else {
-				System.out.println("You have not enough money to open this Account!"); //for testing
-			}
-		}
-		else {
-			System.out.println("You don't have an account with this IBAN! Please enter a valid IBAN!"); //for testing
+		} else {
+			System.out
+					.println("You don't have an account with this IBAN! Please enter a valid IBAN!"); // for
+																										// testing
 		}
 	}
 
 	public void openDeposit(IClient client, String initiatorIban,
 			double amount, depositType type) {
-		if(allAccounts.containsKey(initiatorIban)) {
-			if(allAccounts.get(initiatorIban).getAmount() >= amount) {
-				if(!client.getClientAccounts().containsKey("Deposit")) {
-					client.getClientAccounts().put("Deposit", new HashMap<String, ArrayList<Account>>());
+		if (allAccounts.containsKey(initiatorIban)) {
+			if (allAccounts.get(initiatorIban).getAmount() >= amount) {
+				if (!client.getClientAccounts().containsKey("Deposit")) {
+					client.getClientAccounts().put("Deposit",
+							new HashMap<String, ArrayList<Account>>());
 				}
-				if(type.equals("DepositLongTerm")) {
-					if(!client.getClientAccounts().get("Deposit").containsKey("DepositLongTerm")) {
-						client.getClientAccounts().get("CurrentAccount").put("DepositLongTerm", new ArrayList<Account>());
+				if (type.equals("DepositLongTerm")) {
+					if (!client.getClientAccounts().get("Deposit")
+							.containsKey("DepositLongTerm")) {
+						client.getClientAccounts()
+								.get("CurrentAccount")
+								.put("DepositLongTerm",
+										new ArrayList<Account>());
 					}
-					client.getClientAccounts().get("CurrentAccount").get("DepositLongTerm").add(new DepositLongTerm(amount));
-					allAccounts.get(initiatorIban).removeMoneyFromAccount(amount);
-				}
-				else if(type.equals("DepositShortTerm")) {
-					if(!client.getClientAccounts().get("Deposit").containsKey("DepositShortTerm")) {
-						client.getClientAccounts().get("CurrentAccount").put("DepositShortTerm", new ArrayList<Account>());
+					client.getClientAccounts().get("CurrentAccount")
+							.get("DepositLongTerm")
+							.add(new DepositLongTerm(amount));
+					allAccounts.get(initiatorIban).removeMoneyFromAccount(
+							amount);
+				} else if (type.equals("DepositShortTerm")) {
+					if (!client.getClientAccounts().get("Deposit")
+							.containsKey("DepositShortTerm")) {
+						client.getClientAccounts()
+								.get("CurrentAccount")
+								.put("DepositShortTerm",
+										new ArrayList<Account>());
 					}
-					client.getClientAccounts().get("CurrentAccount").get("DepositShortTerm").add(new DepositShortTerm(amount));
-					allAccounts.get(initiatorIban).removeMoneyFromAccount(amount);
+					client.getClientAccounts().get("CurrentAccount")
+							.get("DepositShortTerm")
+							.add(new DepositShortTerm(amount));
+					allAccounts.get(initiatorIban).removeMoneyFromAccount(
+							amount);
 				}
+			} else {
+				System.out
+						.println("You have not enough money to open this Deposit!"); // for
+																						// testing
 			}
-			else {
-				System.out.println("You have not enough money to open this Deposit!"); //for testing
-			}
-		}
-		else {
-			System.out.println("You don't have an account with this IBAN! Please enter a valid IBAN!"); //for testing
+		} else {
+			System.out
+					.println("You don't have an account with this IBAN! Please enter a valid IBAN!"); // for
+																										// testing
 		}
 
 	}
