07dae5d05146cd5301406d6ad6c646c57ea459b9
b.naskov <boni.mislyashki@gmail.com>
2014-12-10 00:09:36 +0200

    spring-banking

---
+++ spring-banking/WebContent/WEB-INF/tiles/footer.jsp
@@ -0,0 +1,1 @@
+<span>Created by IT-Talents 2014 All rights reserved</span>
---
+++ spring-banking/src/com/banking/spring/web/dao/Account.java
@@ -0,0 +1,104 @@
+package com.banking.spring.web.dao;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Size;
+
+public class Account {
+
+	private int id;
+
+	@Size(min = 22, max = 22)
+	private String iban;
+
+	@Min(0)
+	private double amount;
+
+	private User user;
+
+	public Account() {
+		this.user = new User();
+	}
+
+	public Account(int id, User user, String iban, double amount) {
+		this.id = id;
+		this.user = user;
+		this.iban = iban;
+		this.amount = amount;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getIban() {
+		return iban;
+	}
+
+	public void setIban(String iban) {
+		this.iban = iban;
+	}
+
+	public double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		long temp;
+		temp = Double.doubleToLongBits(amount);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((iban == null) ? 0 : iban.hashCode());
+		result = prime * result + ((user == null) ? 0 : user.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Account other = (Account) obj;
+		if (Double.doubleToLongBits(amount) != Double
+				.doubleToLongBits(other.amount))
+			return false;
+		if (iban == null) {
+			if (other.iban != null)
+				return false;
+		} else if (!iban.equals(other.iban))
+			return false;
+		if (user == null) {
+			if (other.user != null)
+				return false;
+		} else if (!user.equals(other.user))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Account [iban=" + iban + ", amount=" + amount + ", user="
+				+ user + "]";
+	}
+
+}
