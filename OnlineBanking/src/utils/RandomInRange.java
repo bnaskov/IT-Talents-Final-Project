a3405ff355ed6a297c8c24b16dd9caa02d601b60
Georgi Georgiev <ggeorgieev@gmail.com>
2014-12-15 13:12:00 +0200

    changes to the generator

--- OnlineBanking/src/utils/RandomInRange.java
+++ OnlineBanking/src/utils/RandomInRange.java
@@ -7,6 +7,7 @@ public class RandomInRange {
 	public static int randIntRange(int min, int max) {
 		Random rand = new Random();
 		int randomNum = rand.nextInt((max - min) + 1) + min;
+		int abc;
 
 		return randomNum;
 	}
