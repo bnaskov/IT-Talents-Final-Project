3c01c86d0140361d552b50313a39f7ed95e0d09b
Georgi Georgiev <ggeorgieev@gmail.com>
2014-12-15 13:11:39 +0200

    changes to Account

--- OnlineBanking/src/products/accounts/Account.java
+++ OnlineBanking/src/products/accounts/Account.java
@@ -29,4 +29,31 @@ public abstract class Account extends BankProduct {
 	public void removeMoneyFromAccount(double amount) {
 		this.amount -= amount;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((iban == null) ? 0 : iban.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Account other = (Account) obj;
+		if (iban == null) {
+			if (other.iban != null)
+				return false;
+		} else if (!iban.equals(other.iban))
+			return false;
+		return true;
+	}
+	
+	
 }
